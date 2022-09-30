12daf55db5112b31e94a99f554ecdce1c8ef6fd2
Noah Kuzyk <coolxnoah@gmail.com>
2022-09-30 09:54:00 -0400

    Fixed a small movement bug

--- BobbleheadWars/Assets/Scripts/PlayerController.cs
+++ BobbleheadWars/Assets/Scripts/PlayerController.cs
@@ -18,13 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.SimpleMove(moveDirection * moveSpeed);
+
+        //Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
     }
 
     void FixedUpdate() {
+        Vector3 pos = transform.position;
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
         if (moveDirection == Vector3.zero)
         {
             // TODO
@@ -43,14 +45,12 @@ public class PlayerController : MonoBehaviour
                 currentLookTarget = hit.point;
             }
             // 1
-            Vector3 targetPosition = new Vector3(hit.point.x,
-             transform.position.y, hit.point.z);
+            Vector3 targetPosition = new Vector3(hit.point.x,transform.position.y, hit.point.z);
             // 2
-            Quaternion rotation = Quaternion.LookRotation(targetPosition -
-             transform.position);
+            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
             // 3
-            transform.rotation = Quaternion.Lerp(transform.rotation,
-             rotation, Time.deltaTime * 10.0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+
         }
     }
 

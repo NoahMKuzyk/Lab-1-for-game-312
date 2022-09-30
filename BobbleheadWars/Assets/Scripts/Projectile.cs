8f28daff9686e8f93e25e3cb617181821b748f53
Noah Kuzyk <coolxnoah@gmail.com>
2022-09-29 23:38:47 -0400

    Added raycasting and remaining physics

---
+++ BobbleheadWars/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}

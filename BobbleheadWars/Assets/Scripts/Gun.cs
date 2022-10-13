c9a40a0220a3ca55ec73bd75ac7d37a0711dc423
Noah Kuzyk <coolxnoah@gmail.com>
2022-10-12 23:06:07 -0400

    Chpt. 7: Changed firing code to aid powerup implementation; added powerup functionality

--- BobbleheadWars/Assets/Scripts/Gun.cs
+++ BobbleheadWars/Assets/Scripts/Gun.cs
@@ -6,16 +6,25 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    private AudioSource audioSource;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentTime += Time.deltaTime;
+        if (currentTime > upgradeTime && isUpgraded == true)
+        {
+            isUpgraded = false;
+        }
         if (Input.GetMouseButtonDown(0))
         {
             if (!IsInvoking("fireBullet"))
@@ -28,14 +37,44 @@ public class Gun : MonoBehaviour
             CancelInvoke("fireBullet");
         }
     }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
     void fireBullet()
     {
-        // 1
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        // 2
-        bullet.transform.position = launchPosition.position;
-        // 3
-        bullet.GetComponent<Rigidbody>().velocity =
-        transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity =
+            (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity =
+            ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+
     }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
+    }
+
 }
--- BobbleheadWars/Assets/Scripts/SoundManager.cs
+++ BobbleheadWars/Assets/Scripts/SoundManager.cs
@@ -37,6 +37,11 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+
     // Update is called once per frame
     void Update()
     {
